0cf05b3503ad5f071bf92997c89f75c19e5d75f3
ezravm <33087661+ezravm@users.noreply.github.com>
2018-09-03 15:26:41 +0200

    added question & answer using node localstorage
    server.js heeft modules voor ask en answer, gebruikt node localstorage.
    werkt met beperkte array

--- express-eightball/server.js
+++ express-eightball/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000');
 });
@@ -10,3 +12,50 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch');
+}
+
+var answers = ["it is certain", "as i see it, yes", "reply hazy, try again", "dont count on it"];
+
+
+// Redirect to ask
+app.get('/', (req, res) => {
+   res.redirect('/ask')
+})
+
+// Show the ask form
+app.get('/ask', (req, res) => {
+   res.render('ask.ejs', { product: '' })
+})
+
+
+// Find a question
+app.post('/ask', (req, res) => {
+ var query = req.body.question
+ console.log(query);
+
+
+
+if (localStorage.getItem(query) == null) {
+  var random = getRandomInt(3);
+  console.log(random);
+  console.log("data uit array:");
+  var store = answers[random];
+  console.log(store);
+  localStorage.setItem(query,store);
+  res.render('answer.ejs',{answer: store} )
+}
+else {
+  var store = localStorage.getItem(query);
+  res.render('answer.ejs',{answer: store});
+}
+})
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
